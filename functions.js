267c4f43cec16e89ca99da508203ea25486cc4fd
Nicolae Matei <nmatei@sdl.com>
2021-01-14 21:00:00 +0200

    add hide fn

--- functions.js
+++ functions.js
@@ -1,47 +1,40 @@
+function hide(id) {
+    var el = document.getElementById(id);
+    el.style.display = 'none';
+}
+
 function showHomePage() {
-    var skills = document.getElementById('skills');
-    skills.style.display = 'none';
-    var languages = document.getElementById('languages');
-    languages.style.display = 'none';
-    var projects = document.getElementById('projects');
-    projects.style.display = 'none';
+    hide("skills");
+    hide("languages");
+    hide("projects");
 
     var page = document.getElementById('home');
     page.style.display = '';
 }
 
 function showSkillsPage() {
-    var home = document.getElementById('home');
-    home.style.display = 'none';
-    var languages = document.getElementById('languages');
-    languages.style.display = 'none';
-    var projects = document.getElementById('projects');
-    projects.style.display = 'none';
+    hide('home');
+    hide('languages');
+    hide('projects');
 
     var page = document.getElementById('skills');
     page.style.display = '';
 }
 
 function showLanguagesPage() {
-    var skills = document.getElementById('skills');
-    skills.style.display = 'none';
-    var home = document.getElementById('home');
-    home.style.display = 'none';
-    var projects = document.getElementById('projects');
-    projects.style.display = 'none';
+    hide('skills');
+    hide('home');
+    hide('projects');
 
     var page = document.getElementById('languages');
     page.style.display = '';
 }
 
 function showProjectsPage() {
-    var skills = document.getElementById('skills');
-    skills.style.display = 'none';
-    var languages = document.getElementById('languages');
-    languages.style.display = 'none';
-    var home = document.getElementById('home');
-    home.style.display = 'none';
-
+    hide('skills');
+    hide('languages');
+    hide('home');
+    
     var page = document.getElementById('projects');
     page.style.display = '';
 }
