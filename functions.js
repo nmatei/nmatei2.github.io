cdc27a96b09c6d5773bbad7b935be1054c194a91
Nicolae Matei <nmatei@sdl.com>
2021-01-14 21:07:58 +0200

    add show fn

--- functions.js
+++ functions.js
@@ -2,39 +2,34 @@ function hide(id) {
     var el = document.getElementById(id);
     el.style.display = 'none';
 }
+function show(id) {
+    document.getElementById(id).style.display = '';
+}
 
 function showHomePage() {
     hide("skills");
     hide("languages");
     hide("projects");
-
-    var page = document.getElementById('home');
-    page.style.display = '';
+    show('home');
 }
 
 function showSkillsPage() {
     hide('home');
     hide('languages');
     hide('projects');
-
-    var page = document.getElementById('skills');
-    page.style.display = '';
+    show('skills');
 }
 
 function showLanguagesPage() {
     hide('skills');
     hide('home');
     hide('projects');
-
-    var page = document.getElementById('languages');
-    page.style.display = '';
+    show('languages');
 }
 
 function showProjectsPage() {
     hide('skills');
     hide('languages');
     hide('home');
-    
-    var page = document.getElementById('projects');
-    page.style.display = '';
+    show('projects');
 }
