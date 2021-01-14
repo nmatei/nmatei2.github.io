7fdf6d90b18dc303fdebde2108cbde9b7e135d39
Nicolae Matei <nmatei@sdl.com>
2021-01-14 21:18:51 +0200

    add hideAllPages fn

--- functions.js
+++ functions.js
@@ -2,34 +2,34 @@ function hide(id) {
     var el = document.getElementById(id);
     el.style.display = 'none';
 }
+
 function show(id) {
     document.getElementById(id).style.display = '';
 }
 
-function showHomePage() {
+function hideAllPages() {
+    hide('home');
     hide("skills");
     hide("languages");
     hide("projects");
+}
+
+function showHomePage() {
+    hideAllPages();
     show('home');
 }
 
 function showSkillsPage() {
-    hide('home');
-    hide('languages');
-    hide('projects');
+    hideAllPages();
     show('skills');
 }
 
 function showLanguagesPage() {
-    hide('skills');
-    hide('home');
-    hide('projects');
+    hideAllPages();
     show('languages');
 }
 
 function showProjectsPage() {
-    hide('skills');
-    hide('languages');
-    hide('home');
+    hideAllPages();
     show('projects');
 }
