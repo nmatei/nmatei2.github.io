3f1d0cef463e1c95a7181edba9d0549a61c4ca68
Nicolae Matei <nmatei@sdl.com>
2021-01-21 19:46:51 +0200

    fix hideAllPages

--- functions.js
+++ functions.js
@@ -8,15 +8,12 @@ function show(id) {
 }
 
 function hideAllPages() {
-    var pagesIds = [
-        "home", "skills", "languages", "projects"
-    ];
-    console.debug(pagesIds);
+    var pages = document.querySelectorAll(".page");
 
-    //  initial  ; condition          ; post exectuion
-    for(var i = 0; i < pagesIds.length; i++) {
-        console.info('i = ', i);
-        hide(pagesIds[i]);
+    for(var i = 0; i < pages.length; i++) {
+        var page = pages[i];
+        hide(page.id);
+        //hide(pages[i].id);
     }
 }
 
